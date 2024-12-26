d0bf42fddd1d875997f62a6364fe69f51e581ff3
vengersmoon <vengersmon003@gmail.com>
2024-12-27 00:23:36 +0530

    r

--- src/constructor.java
+++ src/constructor.java
@@ -3,9 +3,9 @@ class  Student{
     String name;
     int age;
 
-    // Student(){                     // constructor declaration same as class name   { DEFAULT constructor }
-    //     System.out.println("constructor called");     
-    // }
+    Student(){                     // constructor declaration same as class name   { DEFAULT constructor }
+        System.out.println("constructor called");     
+    }
 
 //    Student(String name, int age){   // { PARAMETERIZED constructor}
 //         this.name = name;
@@ -13,7 +13,12 @@ class  Student{
 //         System.out.println(name + " " + age);
 //    }
 
+     Student(Student obj1){
 
+        this.name = obj1.name;
+        this.age = obj1.age;
+        System.out.println(name + " " + age);
+     }
 }
 
 public class constructor {
@@ -22,5 +27,13 @@ public class constructor {
         //Student obj = new Student();   // constructor call when object declared     { DEFAULT constructor }
 
         // Student obj = new Student("Piyush", 20);   // { PARAMETERIZED constructor}
+
+        Student obj = new Student();
+        obj.name = "Piyush";
+        obj.age = 20;
+ 
+    Student obj1 = new Student(obj);
+
+
     }
 }
