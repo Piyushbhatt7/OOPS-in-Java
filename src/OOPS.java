530b5f5f4d5c4ac875f31b11557678b9840a62b8
vengersmoon <vengersmon003@gmail.com>
2024-12-26 22:16:36 +0530

    f

--- src/OOPS.java
+++ src/OOPS.java
@@ -9,6 +9,12 @@ class Pen{
 }
 
 public class OOPS {
-
+          
+    public static void main(String[] args) {
+        
+        Pen pen1 = new Pen();
+        pen1.color = "Blue";
+        pen1.type
+    }
     
 }
