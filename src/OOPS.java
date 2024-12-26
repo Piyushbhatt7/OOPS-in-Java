187342354081b550d3d1f9504f46e18b38ecbd47
vengersmoon <vengersmon003@gmail.com>
2024-12-26 23:44:22 +0530

    ff

--- src/OOPS.java
+++ src/OOPS.java
@@ -1,14 +1,26 @@
-class Pen{
+// class Pen{
 
-    String color;
-    String type; // balllpoint , gelpoint
+//     String color;
+//     String type; // balllpoint , gelpoint
 
-    public void write(){
-        System.out.println("Writing somthing..");
-    }
+//     public void write(){
+//         System.out.println("Writing somthing..");
+//     }
 
-    public void printcolor(){
-        System.out.println(this.color);
+//     public void printcolor(){
+//         System.out.println(this.color);
+//     }
+// }
+
+class Student {
+      
+    String name;
+    int age;
+
+    public void name(){
+
+        System.out.println(this.name);
+        System.out.println(this.age);
     }
 }
 
@@ -16,17 +28,22 @@ public class OOPS {
           
     public static void main(String[] args) {
         
-        Pen pen1 = new Pen();
-        pen1.color = "Blue";
-        pen1.type = "Ballpoint";
+        // Pen pen1 = new Pen();
+        // pen1.color = "Blue";
+        // pen1.type = "Ballpoint";
 
-        Pen pen2 = new Pen();
-        pen2.color = "Red";
-        pen2.type = "Gelpoint";
-        pen1.write();
+        // Pen pen2 = new Pen();
+        // pen2.color = "Red";
+        // pen2.type = "Gelpoint";
+        // pen1.write();
 
-        pen1.printcolor();
-        pen2.printcolor();
+        Student stu = new Student();
+        stu.name = "Piyush";
+        stu.age = 20;
+        stu.name();
+
+        // pen1.printcolor();
+        // pen2.printcolor();
     }
     
 }
