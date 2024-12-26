707404ffd8cbc3a4054e63e9e7ff1327703050a1
vengersmoon <vengersmon003@gmail.com>
2024-12-27 00:24:47 +0530

    { COPY constructor }

--- src/constructor.java
+++ src/constructor.java
@@ -14,7 +14,7 @@ class  Student{
 //    }
 
      Student(Student obj1){
-
+                                                //  { COPY constructor }
         this.name = obj1.name;
         this.age = obj1.age;
         System.out.println(name + " " + age);
@@ -26,9 +26,9 @@ public class constructor {
     public static void main(String[] args) {
         //Student obj = new Student();   // constructor call when object declared     { DEFAULT constructor }
 
-        // Student obj = new Student("Piyush", 20);   // { PARAMETERIZED constructor}
+        // Student obj = new Student("Piyush", 20);   // { PARAMETERIZED constructor }
 
-        Student obj = new Student();
+        Student obj = new Student();               // { COPY constructor }
         obj.name = "Piyush";
         obj.age = 20;
  
