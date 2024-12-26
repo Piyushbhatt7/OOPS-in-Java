0ae7e774b8d87bd7a0486b9cb5e9e90f6a8ad268
vengersmoon <vengersmon003@gmail.com>
2024-12-26 22:14:34 +0530

    java oops

---
+++ src/OOPS.java
@@ -0,0 +1,14 @@
+class Pen{
+
+    String color;
+    String type; // balllpoint , gelpoint
+
+    public void write(){
+        System.out.println("Writing somthing.somthing..");
+    }
+}
+
+public class OOPS {
+
+    
+}
