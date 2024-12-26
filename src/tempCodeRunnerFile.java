e1b1f810f842814c31221b4e02f4e8ffc86dce7e
vengersmoon <vengersmon003@gmail.com>
2024-12-26 23:44:17 +0530

    Add initial Pen class definition with commented methods and attributes

---
+++ src/tempCodeRunnerFile.java
@@ -0,0 +1,13 @@
+class Pen{
+
+//     String color;
+//     String type; // balllpoint , gelpoint
+
+//     public void write(){
+//         System.out.println("Writing somthing..");
+//     }
+
+//     public void printcolor(){
+//         System.out.println(this.color);
+//     }
+// }
