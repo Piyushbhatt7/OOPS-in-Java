c333bf65df0c3375a39a8a1c21f296cb6c75ea39
vengersmoon <vengersmon003@gmail.com>
2024-12-27 23:08:41 +0530

    r

--- src/Bank.java
+++ src/Bank.java
@@ -8,5 +8,7 @@ public class Bank {
         Account ac1 = new  Account();
         ac1.name = "Pussy";
         System.out.println(ac1.name);
+        ac1.email = "pussy345@gmail.com";
+        
     }
 }
