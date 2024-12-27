ea50de7c21546c18d1cd96bb99c917128c6206e3
vengersmoon <vengersmon003@gmail.com>
2024-12-27 11:25:17 +0530

    r

---
+++ src/polymorphism.java
@@ -0,0 +1,19 @@
+class myclass {
+
+    public void info(int age){
+        System.out.println(age);
+    }
+
+    public void info(String name) {
+        System.out.println(name);
+    }
+
+    public 
+}
+
+
+
+public class polymorphism {
+    
+
+}
