5a19a03da52f9f7d39d95966674d4eb940f19941
vengersmoon <vengersmon003@gmail.com>
2024-12-27 14:43:04 +0530

    hh

--- src/hierarchialInherit.java
+++ src/hierarchialInherit.java
@@ -1,3 +1,29 @@
+class Shape {
+
+    public void area(){
+        System.out.println("Display area");
+    }
+}
+
+class Triangle extends Shape{
+          public double area(int legth, int height){
+            System.out.println("Triangle");
+              return 0.5 * legth * height;
+          }
+}
+
+
+class EquilateralTriangle extends Triangle {
+          
+    public double  area(int length, int height){
+        System.out.println("EquilateralTriangle");
+             return 0.5 * length * height;
+    }
+}
+
+
+
+
 
 
 
