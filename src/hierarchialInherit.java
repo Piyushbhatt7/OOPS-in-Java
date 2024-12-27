8474b9caf5d0cb78076d0708a584b7705ca9fb6d
vengersmoon <vengersmon003@gmail.com>
2024-12-27 14:48:35 +0530

    55t

--- src/hierarchialInherit.java
+++ src/hierarchialInherit.java
@@ -28,6 +28,9 @@ public class hierarchialInherit {
     public static void main(String[] args) {
         
         Triangle tr1 = new Triangle();
-        
+        System.out.println(tr1.area(12, 1));
+
+        Circle cr = new Circle();
+        System.out.println(cr.area(2));
     }
 }
