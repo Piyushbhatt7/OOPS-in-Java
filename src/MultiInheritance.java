4c442ecbc7b95a99cf505796d851932c0cdaf146
vengersmoon <vengersmon003@gmail.com>
2024-12-27 11:48:19 +0530

    r

--- src/MultiInheritance.java
+++ src/MultiInheritance.java
@@ -1,3 +1,24 @@
+class Shape {
+
+    public void area(){
+        System.out.println("Display area");
+    }
+}
+
+class Triangle extends Shape{
+          public double area(int legth, int height){
+              return 0.5 * legth * height;
+          }
+}
+
+
+class EquilateralTriangle extends Triangle {
+
+}
+
+
+
+
 public class MultiInheritance {
     public static void main(String[] args) {
         
