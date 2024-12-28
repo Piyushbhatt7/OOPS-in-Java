bce72b4882a349bf1df1a0b3b2fda9999f723a54
vengersmoon <vengersmon003@gmail.com>
2024-12-28 11:14:14 +0530

    g

--- src/abstraction.java
+++ src/abstraction.java
@@ -1,7 +1,16 @@
 class Animal{
-    
+
+    public void walk(){
+
+    }
 }
 
+class Horse extends Animal {
+    public void walk(){
+
+        System.out.println("Walk on 4 legs");
+    }
+}
 
 
 public class abstraction {
