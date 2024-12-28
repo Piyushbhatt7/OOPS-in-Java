c83f456b341a99e3fc808aca098862e113e95a9a
vengersmoon <vengersmon003@gmail.com>
2024-12-28 11:31:08 +0530

    f

--- src/pureabstract.java
+++ src/pureabstract.java
@@ -1,5 +1,13 @@
 interface Animal {
-    
+
+    public void walk();
+                                      // can't use non abstract function
+}
+
+class Horse implements Animal{
+    public void walk() {
+         System.out.println("Walks on 5 legs");
+    }
 }
 
 public class pureabstract {
