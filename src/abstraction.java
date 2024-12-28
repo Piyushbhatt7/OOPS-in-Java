8656367853d72e60a53d72a7abdc110c2bb4f503
vengersmoon <vengersmon003@gmail.com>
2024-12-28 11:28:34 +0530

    d

--- src/abstraction.java
+++ src/abstraction.java
@@ -21,6 +21,7 @@ public class abstraction {
     public static void main(String[] args) {
         Horse h1 = new Horse();
         h1.walk();
+        
         Chicken ch = new Chicken();
         ch.walk();
 
---
+++ src/pureabstract.java
@@ -0,0 +1,9 @@
+interface Animal {
+    
+}
+
+public class pureabstract {
+    public static void main(String[] args) {
+        
+    }
+}
