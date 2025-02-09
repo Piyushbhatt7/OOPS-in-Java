7d6c21a06bc1c3cad3874c1489b3087147692064
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-09 22:59:51 +0530

    Inheritance in java

--- src/Inheitance.java
+++ src/Inheitance.java
@@ -1,17 +1,18 @@
+
 class Shape {
 
     String color;
 
 }
 
-class Triangle extends Shape{
+class Triangle extends Shape {
 
 }
 
 public class Inheitance {
-    
+
     public static void main(String[] args) {
-        
+
         Triangle t1 = new Triangle();
         t1.color = "red";
         System.out.println(t1.color);
