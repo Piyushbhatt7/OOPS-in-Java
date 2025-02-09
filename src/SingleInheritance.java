781feeb5482f43808b0f8071013c0952d132728c
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-09 23:01:01 +0530

    single-inheritance

--- src/SingleInheritance.java
+++ src/SingleInheritance.java
@@ -1,17 +1,20 @@
+
 class Shape {
 
-    public void area(){
+    public void area() {
         System.out.println("Display area");
     }
 }
 
-class Triangle extends Shape{
-          public double area(int legth, int height){
-              return 0.5 * legth * height;
-          }
+class Triangle extends Shape {
+
+    public double area(int legth, int height) {
+        return 0.5 * legth * height;
+    }
 }
 
 public class SingleInheritance {
+
     public static void main(String[] args) {
         Triangle obj = new Triangle();
         obj.area();
