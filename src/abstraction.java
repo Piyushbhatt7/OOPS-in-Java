5287dbdbec2562d98b8228e9b2fb428f1205a6ff
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-09 23:01:32 +0530

    abstraction in java

--- src/abstraction.java
+++ src/abstraction.java
@@ -1,30 +1,32 @@
-abstract  class Animal{
 
-    abstract  void walk();         // abstract function
+abstract class Animal {
+
+    abstract void walk();         // abstract function
 }
 
 class Horse extends Animal {
-    public void walk(){
+
+    public void walk() {
 
         System.out.println("Walk on 4 legs");
     }
 }
 
 class Chicken extends Animal {
-    public void walk(){
+
+    public void walk() {
         System.out.println("Walks on 2 legs");
     }
 }
 
-
 public class abstraction {
+
     public static void main(String[] args) {
         Horse h1 = new Horse();
         h1.walk();
-        
+
         Chicken ch = new Chicken();
         ch.walk();
 
-        
     }
 }
