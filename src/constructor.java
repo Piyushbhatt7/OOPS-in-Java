5059be44245cc9a497de54adfb75361f456986d3
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-09 23:02:11 +0530

    default, parameterized, copy constructor

--- src/constructor.java
+++ src/constructor.java
@@ -1,10 +1,11 @@
-class  Student{
-       
+
+class Student {
+
     String name;
     int age;
 
-    Student(){                     // constructor declaration same as class name   { DEFAULT constructor }
-        System.out.println("constructor called");     
+    Student() {                     // constructor declaration same as class name   { DEFAULT constructor }
+        System.out.println("constructor called");
     }
 
 //    Student(String name, int age){   // { PARAMETERIZED constructor}
@@ -12,27 +13,25 @@ class  Student{
 //         this.age = age;
 //         System.out.println(name + " " + age);
 //    }
-
-     Student(Student obj1){
-                                                //  { COPY constructor }
+    Student(Student obj1) {
+        //  { COPY constructor }
         this.name = obj1.name;
         this.age = obj1.age;
         System.out.println(name + " " + age);
-     }
-} 
+    }
+}
 
 public class constructor {
-    
+
     public static void main(String[] args) {
         //Student obj = new Student();   // constructor call when object declared     { DEFAULT constructor }
 
         // Student obj = new Student("Piyush", 20);   // { PARAMETERIZED constructor }
-
         Student obj = new Student();                   // { COPY constructor }
         obj.name = "Piyush";
         obj.age = 20;
- 
-    Student obj1 = new Student(obj);
+
+        Student obj1 = new Student(obj);
 
     }
 }
