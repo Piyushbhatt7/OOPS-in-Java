5a7fdf0e47569aa5bcf901be98172bb202a5a0c7
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-09 23:02:45 +0530

    hierarichal inheritance

--- src/hierarchialInherit.java
+++ src/hierarchialInherit.java
@@ -1,32 +1,31 @@
+
 class Shape {
 
-    public void area(){
+    public void area() {
         System.out.println("Display area");
     }
 }
 
-class Triangle extends Shape{
-          public double area(int length, int height){
-            System.out.println("Triangle");
-              return 0.5 * length * height;
-          }
-}
+class Triangle extends Shape {
 
-
-class Circle extends Shape {
-          
-    public double  area(int r){
-        System.out.println("Circle");
-             return (3.14) *r*r; 
+    public double area(int length, int height) {
+        System.out.println("Triangle");
+        return 0.5 * length * height;
     }
 }
 
+class Circle extends Shape {
 
-
+    public double area(int r) {
+        System.out.println("Circle");
+        return (3.14) * r * r;
+    }
+}
 
 public class hierarchialInherit {
+
     public static void main(String[] args) {
-        
+
         Triangle tr1 = new Triangle();
         System.out.println(tr1.area(12, 1));
 
