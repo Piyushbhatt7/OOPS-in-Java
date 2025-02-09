14316fd84c623eafa3fccc93b2af2206e7b05c35
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-09 23:00:27 +0530

    multi-level inheritance

--- src/MultiInheritance.java
+++ src/MultiInheritance.java
@@ -1,30 +1,29 @@
+
 class Shape {
 
-    public void area(){
+    public void area() {
         System.out.println("Display area");
     }
 }
 
-class Triangle extends Shape{
-          public double area(int legth, int height){
-            System.out.println("Triangle");
-              return 0.5 * legth * height;
-          }
-}
+class Triangle extends Shape {
 
-
-class EquilateralTriangle extends Triangle {
-          
-    public double  area(int length, int height){
-        System.out.println("EquilateralTriangle");
-             return 0.5 * length * height;
+    public double area(int legth, int height) {
+        System.out.println("Triangle");
+        return 0.5 * legth * height;
     }
 }
 
+class EquilateralTriangle extends Triangle {
 
-
+    public double area(int length, int height) {
+        System.out.println("EquilateralTriangle");
+        return 0.5 * length * height;
+    }
+}
 
 public class MultiInheritance {
+
     public static void main(String[] args) {
         EquilateralTriangle et = new EquilateralTriangle();
         et.area();
